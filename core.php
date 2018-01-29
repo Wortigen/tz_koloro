e321e9f0d19bb2b5ec31173afc5a01aa0fb74ec9
Wortigen <wortigenwts@gmail.com>
2018-01-29 11:37:20 +0200

    initial_skeleton

---
+++ core.php
@@ -0,0 +1,22 @@
+<?php
+class Core
+{
+	function PageBody($page)
+	{
+		switch($page)
+		{
+			case "Manager":
+			break;
+			case "Project":
+			break;
+			case "list_Manager":
+			break;
+			case "list_Project":
+			break;
+			default:
+			break;
+		}
+	}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include('core.php');
+
+$site = new Core;
+
+?>
